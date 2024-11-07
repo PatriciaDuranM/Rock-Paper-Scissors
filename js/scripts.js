1a1269dc7c70b3aad54f335b088664e9d0108af6
PatriciaDuranM <patduran16@gmail.com>
2024-11-07 20:22:31 +0100

    update

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/*INSTRUCCIONES
+
+- Localizar los elementos implicados del DOM
+- Crear los datos del programa necesarios
+
+FLUJO DEL PROGRAMA
+ 
+  - Detectar dónde hacemos click (Si tenéis problemas al hacer click
+porque detectáis cosas que no os interesan, podéis usar la propiedad
+"pointer-events:none" en CSS para facilitaros la vida)
+    - Guardar nuestra jugada
+    - Generar una jugada aleatoria para el ordenador y guardarla
+    - Comparar jugadas
+    - Mostrar resultado
+    - Asignar puntos*/
